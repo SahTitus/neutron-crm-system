6c6012d23c6cbbf57b6270eff2d5a9b91207b3bd
SahTitus <sahtitus58@gmail.com>
2024-08-17 17:53:09 +0000

    updates, sales, customers, settings, campaigns, dashboard, leads pages

--- db/index.js
+++ db/index.js
@@ -1,29 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const dbName = process.env.MONGODB_DB;
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// let cached = global.mongoose || { conn: null, promise: null };
-
-// const connectDb = async () => {
-//   if (cached.conn) return cached.conn;
-
-//   if (!MONGODB_URI) {
-//     throw new Error('MONGODB_URI is missing');
-//   }
-
-//   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-//     dbName,
-//     bufferCommands: false,
-//   });
-
-//   cached.conn = await cached.promise;
-
-//   return cached.conn;
-// };
-
-// export { connectDb };
-
 import mongoose from 'mongoose';
 
 // Load environment variables
