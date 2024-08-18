28b215fd500d982a3748cedbf9fd8a124dfad903
SahTitus <sahtitus58@gmail.com>
2024-08-18 07:38:10 +0000

    auth fixes

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  console.log(token, process.env.NEXTAUTH_SECRET)
+  console.log(token,req, process.env.NEXTAUTH_SECRET)
 
   if (!token) {
     const signInUrl = new URL('/auth', req.url);
